b6c7f8f667d005229359cdb3ed0f6e71897c259b
Juji.cs <61978075+kylsio@users.noreply.github.com>
2024-03-06 15:00:29 +0100

    proto mercredi fin

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using Cinemachine;
+
 
 public class PlayerController : MonoBehaviour
 {
@@ -6,7 +8,13 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10.0f;
     public int score = 100000;
     public float frollbo = 25.0f;
-
+    public int n = 0;
+    public bool isboosingout;
+    public bool isslowingout;
+    public bool isboosingin;
+    public CinemachineVirtualCamera vcam;
+    public GameObject winui;
+    public GameObject loseui;
     private Rigidbody rb;
     private bool isGrounded;
     private const float LengthMultiplier = 4f;
@@ -18,14 +26,68 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        if (isboosingout == true)
+        {
+            if (n <= 100)
+            {
+                movementSpeed += 0.5f;
+                n += 1;
+            }
+            else if (n > 100 && n < 200)
+            {
+                movementSpeed += 0.2f;
+                n += 1;
+            }
+            else
+            {
+                n = 0;
+                isboosingout = false;
+            }
+        }
+        if (isboosingout == false && movementSpeed != 15.0f)
+        {
+            float interpolationFactor = 1.5f;
+            movementSpeed = Mathf.Lerp(movementSpeed, 15.0f, interpolationFactor * Time.deltaTime);
+        }
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.1f);
+
+        if(score <= -1)
+        {
+            loseui.SetActive(true);
+        }
+
+
+        if (isslowingout == true)
+        {
+            if (n <= 100)
+            {
+                movementSpeed += -0.01f;
+                n += 1;
+                vcam.m_Lens.FieldOfView -= 0.25f;
+            }
+            else if (n > 100 && n < 200)
+            {
+                movementSpeed += -0.005f;
+                n += 1;
+            }
+            else
+            {
+                n = 0;
+                isslowingout = false;
+            }
+        }
+        if (isslowingout == false && movementSpeed != 15.0f)
+        {
+            float interpolationFactor = 0.8f;
+            vcam.m_Lens.FieldOfView = Mathf.Lerp(vcam.m_Lens.FieldOfView, 60f, interpolationFactor * Time.deltaTime);
+            movementSpeed = Mathf.Lerp(movementSpeed, 15.0f, interpolationFactor * Time.deltaTime);
+        }
         isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.1f);
 
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 moveDirection = new Vector3(horizontalInput, 0.0f, verticalInput);
         rb.velocity = new Vector3(moveDirection.x * movementSpeed, rb.velocity.y , moveDirection.z * movementSpeed);;
-
-        
         Debug.Log(score);
 
     }
@@ -35,10 +97,21 @@ public class PlayerController : MonoBehaviour
         if(other.gameObject.CompareTag("dmgin"))
         {
             score -= 100;
+            isslowingout = true;
         }
         else if (other.gameObject.CompareTag("froll"))
         {
             score += 100;
+            isboosingout = true;
+        }
+        else if (other.gameObject.CompareTag("froll")&& other.gameObject.CompareTag("dmgin"))
+        {
+            score -= 100;
+            isslowingout = true;
+        }
+        else if (other.gameObject.CompareTag("winwall"))
+        {
+            winui.SetActive(true);
         }
 
     }
