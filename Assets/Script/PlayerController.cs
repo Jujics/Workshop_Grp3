ce987c98ac049117033756773ddc12ef2264fb39
Juji.cs <61978075+Jujics@users.noreply.github.com>
2024-03-06 22:24:28 +0100

    :)

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -78,10 +78,19 @@ public class PlayerController : MonoBehaviour
         isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.1f);
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
+        Debug.Log("horisontal " + horizontalInput + " vertical " + verticalInput);
+        if (horizontalInput > 0.30f)
+        {
+            horizontalInput = 0.30f;
+        }
+        else if (horizontalInput < -0.30f)
+        {
+            horizontalInput = 0.30f;
+        }
+        Debug.Log("horisontal " + horizontalInput + " vertical " + verticalInput);
         verticalInput = Mathf.Max(0.0f, verticalInput);
         Vector3 moveDirection = new Vector3(horizontalInput, 0.0f, verticalInput);
-        rb.velocity = new Vector3(moveDirection.x * movementSpeed, rb.velocity.y, moveDirection.z * movementSpeed);
-        Debug.Log(score);   
+        rb.velocity = new Vector3(moveDirection.x * movementSpeed, rb.velocity.y, moveDirection.z * movementSpeed);   
     }
     void OnTriggerEnter(Collider other)
     {
