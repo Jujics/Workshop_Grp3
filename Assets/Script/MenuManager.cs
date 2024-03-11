ff2d1b6a6aec05a6533646cf0de1ab5a7f6a42bf
Juji.cs <61978075+Jujics@users.noreply.github.com>
2024-03-11 11:56:50 +0100

    Menu

--- Assets/Script/MenuManager.cs
+++ Assets/Script/MenuManager.cs
@@ -6,8 +6,11 @@ public class MenuManager : MonoBehaviour
 {
     public int lenclig;
     public GameObject txtclig;
+    public GameObject Mainmn;
+    public GameObject Startmn;
     public bool hasclic;
     int i = 0;
+    int ih = 0;
 
     private bool isonmain;
     void Start()
@@ -18,12 +21,24 @@ public class MenuManager : MonoBehaviour
 
     void Update()
     {
-        if (hasclic == true)
+        if (Input.GetKey("Lngame") == 1.0f)
+        {
+            hasclic = true;
+        }
+        if (hasclic == true && lenclig == 600)
         {
             lenclig /= 3;
-            hasclic = false;
         }
         i += 1;
+        if (hasclic == true)
+        {
+            ih += 1;
+            if(ih == 600)
+            {
+                Mainmn.SetActive(true);
+                Startmn.SetActive(false);
+            }
+        }
         if(i == lenclig)
         {
             if (txtclig.activeSelf)
