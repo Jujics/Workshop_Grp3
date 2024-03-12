b0a68c1bb559483d5abfca40b5ca3694da9bc6f0
Juji.cs <61978075+Jujics@users.noreply.github.com>
2024-03-12 13:22:47 +0100

    Update walladv.cs

--- Assets/Script/walladv.cs
+++ Assets/Script/walladv.cs
@@ -7,7 +7,9 @@ public class walladv : MonoBehaviour
     public int wallsp;
     public GameObject player; 
     public bool Critwall;
+    public int i;
     public bool Nearwall;
+    public bool Hasfirewall;
     private Rigidbody rb;
     private Scoremanager vari;
 
@@ -22,19 +24,28 @@ public class walladv : MonoBehaviour
         Vector3 spd = new Vector3(0.0f, 0.0f, 2.0f);
         rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, spd.z * wallsp);
 
-        
-        if (player.transform.position.z >= transform.position.z + 20) 
+        if (Hasfirewall == true)
+        {
+            wallsp = 0;
+            i += 1;
+            if(i == 700)
+            {
+                wallsp = 10;
+                Hasfirewall = false;
+            }
+        }
+        if (player.transform.position.z >= transform.position.z + 20 && Hasfirewall == false) 
         {
             wallsp = 10;
             Nearwall = false;
             Critwall = false;
         }
-        else if (player.transform.position.z <= transform.position.z + 20 && player.transform.position.z >= transform.position.z + 5)
+        else if (player.transform.position.z <= transform.position.z + 20 && player.transform.position.z >= transform.position.z + 5 && Hasfirewall == false)
         {
             Nearwall = true;
             Critwall = false;
         }
-        else if (player.transform.position.z <= transform.position.z + 5)
+        else if (player.transform.position.z <= transform.position.z + 5 && Hasfirewall == false)
         {
             Critwall = true;
             Nearwall = false;
@@ -44,11 +55,18 @@ public class walladv : MonoBehaviour
         {
             vari.score -= 1;
         }
-        else
+        else if(Hasfirewall == false)
         {
             wallsp = 2;
         }
         
 
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("firewall"))
+        {
+            Hasfirewall = true;
+        }
     }
 }
