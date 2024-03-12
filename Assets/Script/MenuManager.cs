be8bf7101238df09d29fe2c71dc70f46a64a1cc6
Juji.cs <61978075+Jujics@users.noreply.github.com>
2024-03-12 15:53:01 +0100

    Choose car

--- Assets/Script/MenuManager.cs
+++ Assets/Script/MenuManager.cs
@@ -11,25 +11,26 @@ public class MenuManager : MonoBehaviour
     public GameObject Startmn;
     public GameObject Options;
     public GameObject Credits;
+    public GameObject Carch;
     public GameObject Button;
+    public GameObject Car1;
+    public GameObject Car2;
     public bool hasclic;
     public int i = 0;
     public int ih = 0;
     public bool onmenu;
+    public int p = 0;
 
     private bool isonmain;
 
     void Start()
     {
-        
+
     }
 
     void Update()
     {
-        if (Input.GetAxis("Lngame") != 0.0f)
-        {
-            hasclic = true;
-        }
+        hasclic = Input.GetAxis("Lngame") != 0.0f;
 
         if (hasclic && lenclig == 600)
         {
@@ -38,7 +39,6 @@ public class MenuManager : MonoBehaviour
 
         i += 1;
 
-        
         if (hasclic && !onmenu)
         {
             ih += 1;
@@ -50,7 +50,16 @@ public class MenuManager : MonoBehaviour
                 EventSystem.current.SetSelectedGameObject(Button);
             }
         }
-
+        if (p == 0)
+        {
+            Car1.SetActive(true);
+            Car2.SetActive(false);
+        }
+        else if (p == 1)
+        {
+            Car1.SetActive(false);
+            Car2.SetActive(true);
+        }
         
         if (i >= lenclig)
         {
@@ -58,29 +67,69 @@ public class MenuManager : MonoBehaviour
             i = 0;
         }
     }
+
     public void OnQuit()
     {
         Application.Quit();
     }
+
     public void OnClicOptions()
     {
         Options.SetActive(true);
         Mainmn.SetActive(false);
     }
+
     public void OnClicCredits()
     {
         Credits.SetActive(true);
         Mainmn.SetActive(false);
     }
+
     public void OnClicBackCre()
     {
         Credits.SetActive(false);
         Mainmn.SetActive(true);
     }
+
     public void OnClicBackOpt()
     {
         Options.SetActive(false);
         Mainmn.SetActive(true);
     }
 
+    public void OnClicBackCar()
+    {
+        Carch.SetActive(false);
+        Mainmn.SetActive(true);
+    }
+
+    public void Onclicleft()
+    {
+        if (p == 0)
+        {
+            p = 1;
+        }
+        else
+        {
+            p = 0;
+        }
+    }
+
+    public void Onclicright()
+    {
+        if (p == 1)
+        {
+            p = 0;
+        }
+        else
+        {
+            p = 1;
+        }
+    }
+
+    public void OnClicChoixvoiture()
+    {
+        Carch.SetActive(true);
+        Mainmn.SetActive(false);
+    }
 }
